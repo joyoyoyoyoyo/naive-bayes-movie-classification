e3c042abddd59763e6cad9ce19266c73c53ffe79
Angel Ortega <an22ortega@gmail.com>
2017-02-20 19:20:07 -0800

    join two sets

--- Tokenizer.py
+++ Tokenizer.py
@@ -5,10 +5,16 @@ class Tokenizer():
     def __init__(self):
         self._regex = re.compile('\\W+')
 
-    def tokenize_sentence(self, sentence, min_word_len, blacklist=[]):
+    def tokenize_sentence(self, sentence, min_word_len=0, blacklist=[]):
         tokens = set([token.lower() for token in self._regex.split(sentence) if len(token) >= min_word_len
                       and token not in blacklist])
+        return tokens
 
+if __name__ == "__main__":
+    tokenizer = Tokenizer()
+    tokens = tokenizer.tokenize_sentence('Hello, my name is Paul')
+    test = tokens | {'hgdfey'}
+    print test
 
 # set([token.lower()
 #      for token in re.findall('\w+', text)
