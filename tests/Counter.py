c4059df1a955bd129ee208859ccdb147ae219ef0
Angel Ortega <an22ortega@gmail.com>
2017-02-20 22:02:24 -0800

    test fixtures

--- tests/Counter.py
+++ tests/Counter.py
@@ -10,5 +10,45 @@ class Counter(unittest.TestCase):
     def testWordCountInDocument(self):
         self
 
+    def testWordNotInVocab(self):
+        self
+
+    def testWordInVocab(self):
+        self
+
+    def testProbWithWordNotInVocab(self):
+        self
+
+    def testProbWithWordInVocab(self):
+        self
+
+    def testUnionNewWord(self):
+        self
+
+    def testUnionOldWord(self):
+        self
+
+    def testAlwaysTheSameSize(self):
+        self
+
+    def testProbabilityIsNotZeroWithLaplaceSmoothing(self):
+        self
+
+    def testProbabilityIsZeroWithoutLaplaceSmoothing(self):
+        self
+
+    def testStopWordsOmitted(self):
+        self
+
+    def testSentenceIsVectorizedToBigrams(self):
+        self
+
+    def testTF_IDF(self):
+        self
+
+    def testWordsAreStemmed(self):
+        self
+
+
 if __name__ == '__main__':
     unittest.main()
