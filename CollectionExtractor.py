3bd7633603532b17ac3da408a4e4b68a7ff6e903
Angel Ortega <an22ortega@gmail.com>
2017-02-21 17:23:01 -0800

    count negative reviews

--- CollectionExtractor.py
+++ CollectionExtractor.py
@@ -12,14 +12,31 @@ def count_pos_reviews(training_mat=np.random.choice([0, 1], size=(3,5), p=[.9, .
     num_pos_reviews = sum(training_mat[:, column_count - 1])
 
     # vectors
-    pos_review_vector = training_mat[:, column_count - 1]
+    labels_vector = training_mat[:, column_count - 1]
 
     print training_mat
     print row_count
     print column_count
     print num_pos_reviews
-    print pos_review_vector
+    print labels_vector
 
+def count_neg_reviews(training_mat=np.random.choice([0,1], size=(3,5), p=[.9,.1])):
+    # values
+    row_count = training_mat.shape[0]
+    column_count = training_mat.shape[1]
+    training_mat[0, column_count - 1] = 1
+    training_mat[1, column_count - 1] = 1
+    num_neg_reviews = row_count - sum(training_mat[:, column_count - 1])
+
+    # vectors
+    labels_vector = training_mat[:, column_count - 1]
+
+    print training_mat
+    print row_count
+    print column_count
+    print num_neg_reviews
+    print labels_vector
 
 
 count_pos_reviews()
+count_neg_reviews()
