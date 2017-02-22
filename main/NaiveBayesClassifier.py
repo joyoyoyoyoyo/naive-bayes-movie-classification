2abf436e1c8797a1d5b84326b7b293086afd945e
Angel Ortega <an22ortega@gmail.com>
2017-02-22 01:33:11 -0800

    BULLSHIT

--- main/NaiveBayesClassifier.py
+++ main/NaiveBayesClassifier.py
@@ -54,14 +54,14 @@ if __name__ == "__main__":
 
         if label == 1:
             freq_positive += term_freq_in_doc_vector
-            prob_positive_given_collection += num_terms_in_doc
+            prob_positive_given_collection += sum(term_freq_in_doc_vector)
         else:
             freq_negative += term_freq_in_doc_vector
-            prob_negative_given_collection += num_terms_in_doc
+            prob_negative_given_collection += sum(term_freq_in_doc_vector)
     print prob_positive_given_collection
     print prob_positive_given_collection
-    prob_pos_vector = np.log(freq_positive + 1 / prob_positive_given_collection)
-    prob_neg_vector = np.log(freq_negative + 1/ prob_negative_given_collection)
+    prob_pos_vector = np.log((freq_positive + 1) / prob_positive_given_collection)
+    prob_neg_vector = np.log((freq_negative + 1) / prob_negative_given_collection)
     print prob_positive_review, prob_pos_vector, prob_neg_vector
         # return prob_positive_review, prob_pos_vector, prob_neg_vector
 
